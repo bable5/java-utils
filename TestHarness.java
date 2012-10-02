ebdeb96450983c6101610cffade6831bc5b83c12
Sarah Kabala <sarahkabala@gmail.com>
2012-10-01 19:41:35 -0500

    Added passing of program arguments to harnessed *.main(...)

--- TestHarness.java
+++ TestHarness.java
@@ -1,32 +1,35 @@
+// A simple harness for delaying entry to Class.main(...) until ENTER is pressed.
+//  Created by Sean Mooney.
+//  Minor changes by Sarah Kabala.
 
 import java.lang.reflect.*;
 
-public class TestHarness{
-    public static void main(String[] args) throws java.io.IOException{
-        //TODO: Checking for no args.
-        if(args.length < 1){
-            System.err.println("Must name the class to run");
+public class TestHarness {
+    public static void main(String[] args) throws java.io.IOException {
+        // TODO: Validate arguments.
+        if(args.length < 1) {
+            System.err.println("Please give the name of a Java class to run in the TestHarness.");
+        } else {
+            String className = args[0];
+            String[] programArgs = null;
+            if (args.length > 1) {
+                programArgs = new String[args.length-1];
+                System.arraycopy(args,1,programArgs,0,programArgs.length);
+            }
+            testHarness(className, programArgs);
         }
-
-        String className = args[0];
-        System.out.println("WARNING NOT COPYING OTHER ARGS FOR PROGRAM!");
-        String[] programArgs = null;// Copy from 1 to end, assuming there are any more args.
-
-        testHarness(className, programArgs);
-
     }
 
-    public static void testHarness(String className, String[] args){
-
-        //jajaja bad practice. TODO: Don't swallow exceptions.
+    public static void testHarness(String className, String[] args) {
+        // TODO: Handle exceptions more carefully.
         try{
-            System.out.println("Press a key to begin");
+            System.out.println("Press ENTER to begin "+className+".");
             System.in.read();
             Class<?> testClass = loadClass(className);
             Method mainMethod = testClass.getDeclaredMethod("main", String[].class);
             mainMethod.invoke(null, (Object) args);
         }catch(Exception ex){
-            System.err.println("Exception encountered");
+            System.err.println("Exception encountered.");
             System.err.println(ex.getMessage());
             ex.printStackTrace();
         }
